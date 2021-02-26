329efb57b3622d43a5fd091d973fb8e4a1392cf5
Anirban Roy <aroyiem@gmail.com>
2021-02-26 14:11:06 +0000

    initial commit

---
+++ src/test/java/com/roy/msscbreweryeureka/MsscBreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roy.msscbreweryeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
